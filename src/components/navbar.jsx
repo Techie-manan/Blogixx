19457d486caf8a6137850ed3f8f724087c34cb95
Manan <aggarwalmanan18@gmail.com>
2025-04-09 17:52:33 +0530

    updt

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,13 +3,7 @@ import React from 'react'
 const navbar = () => {
   return (
     <>
-      <nav className='w-[90vw] h-[10vh] border-b-1  border-gray-200 p-50  rounded-full bg-gray-50'>
-        <ul className='flex justify-between items-center'>
-          <li className='inline-block px-4 py-2 text-gray-700 hover:text-gray-900'>Logo</li>
-          <li className='inline-block px-4 py-2 text-gray-700 hover:text-gray-900'>Search</li>
-          <li className='inline-block px-4 py-2 text-gray-700 hover:text-gray-900'>Add Blog</li>
-        </ul>
-      </nav>
+      
     </>
   )
 }

e5b434dce904e6c16797ece122f5cb9ed096c3ca
Manan <aggarwalmanan18@gmail.com>
2025-04-14 19:53:45 +0530

    13/4

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <div className="line bg-gray-500 m-1 w-[90%] h-0.5 rounded-full justify-self-center"></div>
           <div className="mid flex justify-between">
             <div className='text-gray-600 font-semibold'>Genre</div>
-            <div className='text-gray-400 font-bold'>By {name}</div>
+            <div className='text-gray-400 font-bold'>By name</div>
           </div>
           <div className="content mt-1 text-">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad laudantium quos dolores hic amet unde, quia odio maiores ullam tempore labore alias eum blanditiis at officiis, vel enim esse vitae.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eligendi suscipit dolorum excepturi. Qui esse pariatur sit nam nobis cum necessitatibus, eius facere magnam doloribus voluptates similique error eligendi? Mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta libero rem eum pariatur harum magni repellat suscipit et, nam modi saepe fuga architecto, fugiat esse sint tempora. Fugiat, quasi. Est?
